acd95bb65e4622c1a481f58d9b450b315f603c17
Leonid Shifrin <lshifrin@sprich.com>
2017-08-09 15:05:35 -0400

    fixing build

--- travis_scripts/build_deploy_to_docker_hub.sh
+++ travis_scripts/build_deploy_to_docker_hub.sh
@@ -1,8 +1,9 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 export REPO=$DOCKER_USERNAME/gs-spring-boot
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+#export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+#docker build -f Dockerfile -t $REPO:$COMMIT .
+docker build -f Dockerfile -t $REPO
+#docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO $REPO:$TAG
 docker push $REPO
