99b9a309ffea8e2d0fba0a2ad6acea917e5aad19
Leonid Shifrin <lshifrin@sprich.com>
2017-08-09 15:33:23 -0400

    was missing mvn clean package

--- travis_scripts/build_services.sh
+++ travis_scripts/build_services.sh
@@ -1,8 +1,2 @@
 echo "Building with travis commit of $BUILD_NAME ..."
-#  - export REPO=$DOCKER_USERNAME/gs-spring-boot
-#  - export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-#  - docker build -f Dockerfile -t $REPO:$COMMIT .
-#  - docker tag $REPO:$COMMIT $REPO:$TAG
-#  - docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-#  - docker push $REPO
 mvn clean package docker:build
