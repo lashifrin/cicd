c973fad375f93e22111f8039425a5cd4b48d9ec1
Leonid Shifrin <lshifrin@sprich.com>
2017-08-09 15:15:39 -0400

    missing period

--- travis_scripts/build_deploy_to_docker_hub.sh
+++ travis_scripts/build_deploy_to_docker_hub.sh
@@ -1,9 +1,7 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 export REPO=$DOCKER_USERNAME/gs-spring-boot
-#export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-#docker build -f Dockerfile -t $REPO:$COMMIT .
-docker build -f Dockerfile -t $REPO
-#docker tag $REPO:$COMMIT $REPO:$TAG
+echo "building docker: docker build -f Dockerfile -t $REPO"
+docker build -f Dockerfile -t $REPO .
 docker tag $REPO $REPO:$TAG
 docker push $REPO
