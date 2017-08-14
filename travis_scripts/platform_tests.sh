41cea5cd9c961ba882707afb55f6734b73b8bcae
Leonid Shifrin <lshifrin@sprich.com>
2017-08-14 13:18:12 -0400

    just curl

--- travis_scripts/platform_tests.sh
+++ travis_scripts/platform_tests.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-typeset -i tests_run=0
-function try { this="$1"; }
-function assert {
-    let tests_run+=1
-    [ "$1" = "$2" ] && { echo -n "."; return; }
-    printf "\nFAIL: $this\n'$1' != '$2'\n"; exit 1
-}
+echo "starting platform tests..."
 URL=http://$CONTAINER_IP:8080
-out=$(curl -s -w "%{http_code}" $URL)
-assert "Greetings from Spring Boot/v2!200" "$out"
-echo
-echo "PASS: $tests_run tests run"
+curl $URL
+#out=$(curl -s -w "%{http_code}" $URL)
+#assert "Greetings from Spring Boot/v2!200" "$out"
+#echo
+#echo "PASS: $tests_run tests run"
