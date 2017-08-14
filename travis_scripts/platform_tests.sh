51994e0c388bd78e016fa63de7f242eccad895ae
Leonid Shifrin <lshifrin@sprich.com>
2017-08-14 13:30:44 -0400

    added sleep

--- travis_scripts/platform_tests.sh
+++ travis_scripts/platform_tests.sh
@@ -1,6 +1,7 @@
-echo "starting platform tests..."
-URL=http://$CONTAINER_IP:8080
-curl $URL
+echo "starting platform tests against $CONTAINER_IP"
+#URL=http://$CONTAINER_IP:8080
+sleep 30
+curl http://$CONTAINER_IP:8080
 #out=$(curl -s -w "%{http_code}" $URL)
 #assert "Greetings from Spring Boot/v2!200" "$out"
 #echo
