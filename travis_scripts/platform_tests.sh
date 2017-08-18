7316a672771f4d6cfc2106a1a47347ef14a8227e
Leonid Shifrin <lshifrin@sprich.com>
2017-08-18 09:35:33 -0400

    modifed timeout

--- travis_scripts/platform_tests.sh
+++ travis_scripts/platform_tests.sh
@@ -1,6 +1,6 @@
 echo "starting platform tests against $CONTAINER_IP"
 #URL=http://$CONTAINER_IP:8080
-sleep 30
+sleep 10
 curl http://$CONTAINER_IP:8080/demo
 #out=$(curl -s -w "%{http_code}" $URL)
 #assert "Greetings from Spring Boot/v2!200" "$out"
