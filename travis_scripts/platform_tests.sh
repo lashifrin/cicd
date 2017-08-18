42027d8202969bbc052ed3fb64130572e4b69c2e
Leonid Shifrin <lshifrin@sprich.com>
2017-08-18 08:25:06 -0400

    updated platform test

--- travis_scripts/platform_tests.sh
+++ travis_scripts/platform_tests.sh
@@ -1,7 +1,7 @@
 echo "starting platform tests against $CONTAINER_IP"
 #URL=http://$CONTAINER_IP:8080
 sleep 30
-curl http://$CONTAINER_IP:8080
+curl http://$CONTAINER_IP:8080/demo
 #out=$(curl -s -w "%{http_code}" $URL)
 #assert "Greetings from Spring Boot/v2!200" "$out"
 #echo
